48607a83e0833163c6dd3afb07b247e59067eb6e
Dany Nader <dannynader9@gamil.com>
2017-09-19 14:52:04 +0300

    register with validation

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,55 +1,66 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
-import { DatePicker } from '@ionic-native/date-picker';
-import { RegisterInfo, Params_GetSignup, Codes } from '../../models/models';
-import { DatePipe } from '@angular/common';
-import { basePage } from '../../models/basePage';
-import { DataServiceProvider } from '../../providers/data-service/data-service';
-
+import {Component} from '@angular/core';
+import {NavController, NavParams, Platform} from 'ionic-angular';
+import {DatePicker} from '@ionic-native/date-picker';
+import {RegisterInfo, Params_GetSignup, Codes} from '../../models/models';
+import {DatePipe} from '@angular/common';
+import {basePage} from '../../models/basePage';
+import {DataServiceProvider} from '../../providers/data-service/data-service';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
-  selector: 'page-register',
-  templateUrl: 'register.html',
+    selector: 'page-register',
+    templateUrl: 'register.html',
 })
 export class RegisterPage extends basePage {
-  products: Codes[] = [];
-  registerInfo: RegisterInfo = new RegisterInfo();
-  constructor(public navCtrl: NavController,private api: DataServiceProvider, public navParams: NavParams, private datePicker: DatePicker) {
-    super();       
-    this.GetSignup();
-  }
+    register: FormGroup;
+    products: Codes[] = [];
+    registerInfo: RegisterInfo = new RegisterInfo();
 
-  GetSignup(){
-    let param = new Params_GetSignup();
-    this.api.GetSignup(param).subscribe((data)=> {
-      this.products = data.filter(x=> x.Tbl_Name == '_WebProducts');      
-    });
-  }
+    constructor(public navCtrl: NavController, private api: DataServiceProvider, public formBuilder: FormBuilder, public navParams: NavParams, private datePicker: DatePicker) {
+        super();
+        this.GetSignup();
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
-  }
 
-  CancelHandler() {
-    this.navCtrl.pop();
-  }
+        this.register = this.formBuilder.group({
+            WEB_USER_ID: ['', Validators.required],
+            PRODUCT_CODE: ["", Validators.required],
+            POLICY_NBR: ["", Validators.required],
+            PIN: ["", Validators.required],
+            EMAIL: ["", Validators.required],
+            MOBILE: ["", Validators.required],
 
-  selectExpiryDate() {
-    this.datePicker.show({
-      date: new Date(),
-      mode: 'date',
-      androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
-    }).then(
-      date => {       
-        this.registerInfo.EXPIRY_DATE =  this.FormatDate(date);
-      },
-      err => alert(err)
-      );
-  }
+        });
 
-  Register() {
-    alert(JSON.stringify(this.registerInfo));
-  }
+    }
+
+    GetSignup() {
+        let param = new Params_GetSignup();
+        this.api.GetSignup(param).subscribe((data) => {
+            this.products = data.filter(x => x.Tbl_Name == '_WebProducts');
+        });
+    }
+
+
+    CancelHandler() {
+        this.navCtrl.pop();
+    }
+
+    selectExpiryDate() {
+        this.datePicker.show({
+            date: new Date(),
+            mode: 'date',
+            androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
+        }).then(
+            date => {
+                this.registerInfo.EXPIRY_DATE = this.FormatDate(date);
+            },
+            err => alert(err)
+        );
+    }
+
+    onRegister() {
+        alert(JSON.stringify(this.registerInfo));
+    }
 
 }
